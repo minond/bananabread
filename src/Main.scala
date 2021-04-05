7d68b836832847946611aa5d420563df3ff21f5c
Marcos Minond <minond.marcos@gmail.com>
2021-04-05 12:30:22 -0600

    upgrading sbt and fixing entry point

--- src/Main.scala
+++ src/Main.scala
@@ -2,35 +2,35 @@ package sourdough
 
 import parser.{tokenize, parse, Syntax}
 
+object Main:
+  def main(args: Array[String]) =
+    val syntax = Syntax().withPrefix(1, "-")
+                         .withPrefix(1, "*")
+                         .withInfix(4, "^")
+                         .withInfix(3, "*")
+                         .withInfix(3, "/")
+                         .withInfix(3, "%")
+                         .withInfix(2, "+")
+                         .withInfix(2, "-")
+                         .withPostfix(1, "!")
+                         .withPostfix(1, "++")
 
-def main(args: Array[String]) =
-  val syntax = Syntax().withPrefix(1, "-")
-                       .withPrefix(1, "*")
-                       .withInfix(4, "^")
-                       .withInfix(3, "*")
-                       .withInfix(3, "/")
-                       .withInfix(3, "%")
-                       .withInfix(2, "+")
-                       .withInfix(2, "-")
-                       .withPostfix(1, "!")
-                       .withPostfix(1, "++")
-
-  println(parse("<stdin>", "  4.3 + 54.764", syntax))
-  println(parse("<stdin>", "b + c * 3", syntax))
-  println(parse("<stdin>", "a - a", syntax))
-  println(parse("<stdin>", "a - -a", syntax))
-  println(parse("<stdin>", "-3", syntax))
-  println(parse("<stdin>", "_1+_2", syntax))
-  println(parse("<stdin>", "_1+_2@3", syntax))
-  println(parse("<stdin>", "--_++", syntax))
-  println(parse("<stdin>", "1 + 2 * 3", syntax))
-  println(parse("<stdin>", "1 * 2 + 3!", syntax))
-  println(parse("<stdin>", "a()()()()() + 1", syntax))
-  println(parse("<stdin>", "  4.3 + 3 + -a(1,2,345)!", syntax))
-  println(parse("<stdin>", "abc(1, 2, 3)!", syntax))
-  println(parse("<stdin>", "(1 + 2) * 3", syntax))
-  println(parse("<stdin>", "1 + 2 * 3", syntax))
-  println(parse("<stdin>", "1 + (2 * 3)", syntax)) // Breaks when * is not an infix operator.
-  println(parse("<stdin>", "genfunc(a, b)(c, d, 1 + 2) + (2!)!", syntax))
-  println(parse("<stdin>", "fn(1, func (a, b, c) = 3.14 * a + a * a)(1, 2, 3 + 4) + 5", syntax))
-  println(parse("<stdin>", "*a * *b", syntax)) // Does not parse first prefix operator correctly.
+    println(parse("<stdin>", "  4.3 + 54.764", syntax))
+    println(parse("<stdin>", "b + c * 3", syntax))
+    println(parse("<stdin>", "a - a", syntax))
+    println(parse("<stdin>", "a - -a", syntax))
+    println(parse("<stdin>", "-3", syntax))
+    println(parse("<stdin>", "_1+_2", syntax))
+    println(parse("<stdin>", "_1+_2@3", syntax))
+    println(parse("<stdin>", "--_++", syntax))
+    println(parse("<stdin>", "1 + 2 * 3", syntax))
+    println(parse("<stdin>", "1 * 2 + 3!", syntax))
+    println(parse("<stdin>", "a()()()()() + 1", syntax))
+    println(parse("<stdin>", "  4.3 + 3 + -a(1,2,345)!", syntax))
+    println(parse("<stdin>", "abc(1, 2, 3)!", syntax))
+    println(parse("<stdin>", "(1 + 2) * 3", syntax))
+    println(parse("<stdin>", "1 + 2 * 3", syntax))
+    println(parse("<stdin>", "1 + (2 * 3)", syntax)) // Breaks when * is not an infix operator.
+    println(parse("<stdin>", "genfunc(a, b)(c, d, 1 + 2) + (2!)!", syntax))
+    println(parse("<stdin>", "fn(1, func (a, b, c) = 3.14 * a + a * a)(1, 2, 3 + 4) + 5", syntax))
+    println(parse("<stdin>", "*a * *b", syntax)) // Does not parse first prefix operator correctly.
