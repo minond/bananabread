4634beee8793d6e4fac77aedbc102700a5d996ea
Marcos Minond <minond.marcos@gmail.com>
2021-04-09 21:31:42 -0600

    syntax object helper

--- Parser.scala
+++ Parser.scala
@@ -186,31 +186,35 @@ case class Syntax(
   prefix: Map[String, Int] = Map.empty,
   infix: Map[String, Int] = Map.empty,
   postfix: Map[String, Int] = Map.empty,
-) {
+):
   def isOp(id: ast.Id) = isPrefix(id) || isInfix(id) || isPostfix(id)
   def isOp(lexeme: String) = isPrefix(lexeme) || isInfix(lexeme) || isPostfix(lexeme)
-
   def isPrefix(id: ast.Id) = prefix.keySet.contains(id.lexeme)
   def isPrefix(lexeme: String) = prefix.keySet.contains(lexeme)
   def isInfix(id: ast.Id) = infix.keySet.contains(id.lexeme)
   def isInfix(lexeme: String) = infix.keySet.contains(lexeme)
   def isPostfix(id: ast.Id) = postfix.keySet.contains(id.lexeme)
   def isPostfix(lexeme: String) = postfix.keySet.contains(lexeme)
-
   def prefixPrecedence(id: ast.Id) = postfix.get(id.lexeme).get
   def prefixPrecedence(lexeme: String) = postfix.get(lexeme).get
   def infixPrecedence(id: ast.Id) = infix.get(id.lexeme).get
   def infixPrecedence(lexeme: String) = infix.get(lexeme).get
   def postfixPrecedence(id: ast.Id) = postfix.get(id.lexeme).get
   def postfixPrecedence(lexeme: String) = postfix.get(lexeme).get
-
   def withPrefix(precedence: Int, lexeme: String) = Syntax(prefix + (lexeme -> precedence), infix, postfix)
   def withPrefix(precedence: Int, id: ast.Id) = Syntax(prefix + (id.lexeme -> precedence), infix, postfix)
   def withInfix(precedence: Int, lexeme: String) = Syntax(prefix, infix + (lexeme -> precedence), postfix)
   def withInfix(precedence: Int, id: ast.Id) = Syntax(prefix, infix + (id.lexeme -> precedence), postfix)
   def withPostfix(precedence: Int, lexeme: String) = Syntax(prefix, infix, postfix + (lexeme -> precedence))
   def withPostfix(precedence: Int, id: ast.Id) = Syntax(prefix, infix, postfix + (id.lexeme -> precedence))
-}
+
+object Syntax:
+  def withPrefix(precedence: Int, lexeme: String) = Syntax().withPrefix(precedence, lexeme)
+  def withPrefix(precedence: Int, id: ast.Id) = Syntax().withPrefix(precedence, id)
+  def withInfix(precedence: Int, lexeme: String) = Syntax().withPrefix(precedence, lexeme)
+  def withInfix(precedence: Int, id: ast.Id) = Syntax().withPrefix(precedence, id)
+  def withPostfix(precedence: Int, lexeme: String) = Syntax().withPrefix(precedence, lexeme)
+  def withPostfix(precedence: Int, id: ast.Id) = Syntax().withPrefix(precedence, id)
 
 
 // Parser predicates and combinators
--- test/Utils.scala
+++ test/Utils.scala
@@ -3,18 +3,18 @@ package test
 
 import parser.{parse, Syntax}
 
-val stdOps = Syntax().withPrefix(0, "-")
-                     .withPrefix(0, "∀")
-                     .withPrefix(0, "*")
-                     .withInfix(4, "^")
-                     .withInfix(3, "*")
-                     .withInfix(3, "/")
-                     .withInfix(2, "+")
-                     .withInfix(2, "-")
-                     .withInfix(1, ":")
-                     .withInfix(2, "∈")
-                     .withInfix(1, ">")
-                     .withPostfix(10, "!")
+val stdOps = Syntax.withPrefix(0, "-")
+                   .withPrefix(0, "∀")
+                   .withPrefix(0, "*")
+                   .withInfix(4, "^")
+                   .withInfix(3, "*")
+                   .withInfix(3, "/")
+                   .withInfix(2, "+")
+                   .withInfix(2, "-")
+                   .withInfix(1, ":")
+                   .withInfix(2, "∈")
+                   .withInfix(1, ">")
+                   .withPostfix(10, "!")
 
 def exprsOf(code: String, syntax: Syntax = stdOps) =
   parse("<test>", code, syntax).getOrElse(???).map(_.toString)
