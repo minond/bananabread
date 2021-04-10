939672b6c7fb97d6a316949bef22f30bbc3e83d4
Marcos Minond <minond.marcos@gmail.com>
2021-04-10 14:37:10 -0600

    ty defs

---
+++ Ty.scala
@@ -0,0 +1,15 @@
+package sourdough
+package ty
+
+
+sealed trait Type
+case object Num extends Type
+case object Str extends Type
+case class Var(id: Int) extends Type
+case class Lambda(in: List[Type], out: List[Type]) extends Type
+
+
+val freshIds = LazyList.from(1).sliding(1)
+
+def fresh() =
+    Var(freshIds.next.head)
