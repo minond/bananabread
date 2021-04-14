87948fc73048cec00e3fbaa2da60b35a7908feb0
Marcos Minond <minond.marcos@gmail.com>
2021-04-13 22:53:40 -0600

    switching away from enum adt

--- Ir.scala
+++ Ir.scala
@@ -16,43 +16,35 @@ case class Cond(cond: Ir, pass: Ir, fail: Ir, expr: Expr, ty: Type) extends Ir(e
 case class Let(bindings: List[Binding], body: Ir, expr: Expr, ty: Type) extends Ir(expr, ty)
 
 case class Binding(label: ast.Id, value: Ir, expr: ast.Binding, ty: Type)
-case class TypelessBinding(label: ast.Id, value: Typeless, expr: ast.Binding) extends Print(s"binding: $label value: $value")
 
 /** Simplifies the source tree by removing all operators, leaving only
   * literals, functions, and function application.
   */
-enum Typeless:
-  case Num(num: ast.Num)
-  case Str(str: ast.Str)
-  case Id(id: ast.Id)
-  case App(lambda: Typeless, args: List[Typeless], expr: Expr)
-  case Lambda(params: List[Typeless], body: Typeless, expr: Expr)
-  case Cond(cond: Typeless, pass: Typeless, fail: Typeless, expr: Expr)
-  case Let(bindings: List[TypelessBinding], body: Typeless, expr: Expr)
-
-  override def toString: String = this match
-    case Typeless.Num(num) => s"(num ${num.lexeme})"
-    case Typeless.Str(str) => s"(str ${str.lexeme})"
-    case Typeless.Id(id) => s"(id ${id.lexeme})"
-    case Typeless.App(lambda, args, _) => s"(app lambda: ${lambda} args: (${args.mkString(" ")}))"
-    case Typeless.Lambda(params, body, _) => s"(lambda params: (${params.mkString(" ")}) body: $body)"
-    case Typeless.Cond(cond, pass, fail, _) => s"(if cond: $cond then: $pass else: $fail)"
-    case Typeless.Let(bindings, body, _) => s"(let bindings: (${bindings.mkString(" ")}) body: $body)"
-
 object Typeless:
-  def lift(exprs: List[Expr]): List[Typeless] =
+  sealed trait Base
+  case class Num(num: ast.Num) extends Base with Print(s"(num ${num.lexeme})")
+  case class Str(str: ast.Str) extends Base with Print(s"(str ${str.lexeme})")
+  case class Id(id: ast.Id) extends Base with Print(s"(id ${id.lexeme})")
+  case class App(lambda: Base, args: List[Base], expr: Expr) extends Base with Print(s"(app lambda: ${lambda} args: (${args.mkString(" ")}))")
+  case class Lambda(params: List[Base], body: Base, expr: Expr) extends Base with Print(s"(lambda params: (${params.mkString(" ")}) body: $body)")
+  case class Cond(cond: Base, pass: Base, fail: Base, expr: Expr) extends Base with Print(s"(if cond: $cond then: $pass else: $fail)")
+  case class Let(bindings: List[Binding], body: Base, expr: Expr) extends Base with Print(s"(let bindings: (${bindings.mkString(" ")}) body: $body)")
+
+  case class Binding(label: ast.Id, value: Base, expr: ast.Binding) extends Print(s"binding: $label value: $value")
+
+  def lift(exprs: List[Expr]): List[Base] =
     exprs.map(lift)
 
-  def lift(expr: Expr): Typeless = expr match
-    case num: ast.Num => Typeless.Num(num)
-    case str: ast.Str => Typeless.Str(str)
-    case id: ast.Id => Typeless.Id(id)
-    case ast.App(lambda, args) => Typeless.App(lift(lambda), args.map(lift), expr)
-    case ast.Lambda(params, body) => Typeless.Lambda(params.map(lift), lift(body), expr)
-    case ast.Uniop(op, operand) => Typeless.App(lift(op), List(operand).map(lift), expr)
-    case ast.Binop(op, lhs, rhs) => Typeless.App(lift(op), List(lhs, rhs).map(lift), expr)
-    case ast.Cond(_, cond, pass, fail) => Typeless.Cond(lift(cond), lift(pass), lift(fail), expr)
-    case ast.Let(_, bindings, body) => Typeless.Let(bindings.map(lift), lift(body), expr)
+  def lift(expr: Expr): Base = expr match
+    case num: ast.Num => Num(num)
+    case str: ast.Str => Str(str)
+    case id: ast.Id => Id(id)
+    case ast.App(lambda, args) => App(lift(lambda), args.map(lift), expr)
+    case ast.Lambda(params, body) => Lambda(params.map(lift), lift(body), expr)
+    case ast.Uniop(op, operand) => App(lift(op), List(operand).map(lift), expr)
+    case ast.Binop(op, lhs, rhs) => App(lift(op), List(lhs, rhs).map(lift), expr)
+    case ast.Cond(_, cond, pass, fail) => Cond(lift(cond), lift(pass), lift(fail), expr)
+    case ast.Let(_, bindings, body) => Let(bindings.map(lift), lift(body), expr)
 
-  def lift(binding: ast.Binding): TypelessBinding =
-    TypelessBinding(binding.label, lift(binding.value), binding)
+  def lift(binding: ast.Binding): Binding =
+    Binding(binding.label, lift(binding.value), binding)
