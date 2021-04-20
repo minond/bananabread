93999cf86fbf8e91e7abe721a317a7363c9e336e
Marcos Minond <minond.marcos@gmail.com>
2021-04-19 21:31:00 -0600

    handling new opcodes along with more call conventions

--- Runtime.scala
+++ Runtime.scala
@@ -7,36 +7,42 @@ import opcode.Opcode
 import value.Value
 
 import scala.util.Random
-import scala.collection.mutable.Stack
+import scala.collection.mutable.{Map, Queue}
 
 
-type Instructions = Stack[Instruction]
-val rand = Random.alphanumeric
+type Instructions = Queue[Instruction]
 
 
-sealed trait Section
-case object Main extends Section
-case object Decl extends Section
+class Emitter(section: String = "main", sections: Map[String, Instructions] = Map.empty):
+  def to(section: String) =
+    Emitter(section, sections)
 
-class Emitter(section: Section = Main, main: Instructions = Stack.empty, decl: Instructions = Stack.empty):
-  def to(section: Section) = section match
-    case Main => Emitter(section, main = main)
-    case Decl => Emitter(section, decl = decl)
+  def emit(i: Instruction): Emitter =
+    sections.get(section) match
+      case Some(sec) => sec.addOne(i)
+      case None => sections.update(section, Queue(i))
+    this
 
-  def emit(i: Instruction) = section match
-    case Main => main.push(i)
-    case Decl => decl.push(i)
+  def emit(is: List[Instruction]): Emitter =
+    is.map(emit)
+    this
 
-  def emit(is: List[Instruction]) = section match
-    case Main => is.foreach(i => main.push(i))
-    case Decl => is.foreach(i => decl.push(i))
+  def dump =
+    inst(opcode.Label, value.Id("main")) ++
+    sections.get("main").get ++
+    inst(opcode.Halt) ++
+    (for (sec, instructions) <- sections if sec != "main"
+     yield inst(opcode.Label, value.Id(sec)) ++ instructions).flatten
 
 
 case class Instruction(op: Opcode, args: Value*):
   def toList = List(this)
 
   override def toString = op match
-    case opcode.Label => s"${args(0)}:"
+    case opcode.Label =>
+      if args(0).toString.startsWith("lambda")
+      then s"\n${args(0)}:"
+      else s"${args(0)}:"
     case _ => s"  $op ${args.mkString(", ")}"
 
 
@@ -44,21 +50,28 @@ def inst(op: Opcode, args: Value*) =
   Instruction(op, args:_*).toList
 
 
-def lift(nodes: List[Ir]): List[Instruction] =
-  nodes.flatMap(lift)
+def lift(nodes: List[Ir]): Emitter =
+  val e = Emitter()
+  nodes.foreach(lift(_, e))
+  e
 
-def lift(node: Ir): List[Instruction] = node match
-  case _: tl.Num => push(node, ty.I32)
-  case _: tl.Str => ???
-  case tl.Lambda(params, body, _) => lambda(params, body)
-  case tl.Id(ast.Id(label, _)) => load(label)
-  case tl.App(lambda, args, _) => call(lambda, args)
-  case tl.Cond(cnd, pas, fal, _) => cond(cnd, pas, fal)
-  case tl.Let(bindings, body, _) => let(bindings, body)
+def lift(node: Ir, e: Emitter): Emitter =
+  node match
+    case _: tl.Num => push(node, ty.I32, e)
+    case _: tl.Str => ???
+    case v: tl.Lambda =>
+      // XXX 1
+      lambda(v.params, v.body, e.to(v.ptr))
+      e.emit(inst(opcode.PushPtr, name(v.ptr)))
+    case tl.Id(ast.Id(label, _)) => load(label, e)
+    case tl.App(lambda, args, _) => call(lambda, args, e)
+    case tl.Cond(cnd, pas, fal, _) => cond(cnd, pas, fal, e)
+    case tl.Let(bindings, body, _) => let(bindings, body, e)
+  e
 
 
-def push(node: Ir, typ: ty.Type) = typ match
-  case ty.I32 => inst(opcode.PushI32, value.lift(node))
+def push(node: Ir, typ: ty.Type, e: Emitter) = typ match
+  case ty.I32 => e.emit(inst(opcode.PushI32, value.lift(node)))
   case ty.Str => ???
   case _: ty.Var => ???
   case _: ty.Lambda => ???
@@ -67,52 +80,91 @@ def name(name: String): value.Id =
   value.Id(name)
 
 def unique(name: String): value.Id =
-  value.Id(s"$name.${rand.take(16).mkString}")
+  value.Id(s"$name-${Random.alphanumeric.take(16).mkString}")
 
-def call(lambda: Ir, args: List[Ir]) = lambda match
+def call(lambda: Ir, args: List[Ir], e: Emitter): Unit = lambda match
   case tl.Id(ast.Id(label, _)) if label == "+" =>
-    args.flatMap(lift) ++
-    inst(opcode.Run, name(label))
+    args.foreach(lift(_, e))
+    e.emit(inst(opcode.Run, name(label)))
   case _: tl.Id =>
-    args.flatMap(lift) ++
-    inst(opcode.Call, value.lift(lambda))
+    args.foreach(lift(_, e))
+    e.emit(inst(opcode.Mov, vm.Reg.Rpc, vm.Reg.Pc))
+    e.emit(inst(opcode.Call, value.lift(lambda)))
   case lambda: tl.Lambda =>
-    inst(opcode.PushReg, vm.Reg.Pc) ++
-    args.flatMap(lift) ++
-    inst(opcode.Call, name(lambda.ptr)) ++
-    inst(opcode.Label, name(lambda.ptr)) ++
-    lift(lambda)
+    // e.emit(inst(opcode.PushReg, vm.Reg.Rpc))
+    // e.emit(inst(opcode.PushReg, vm.Reg.Rst))
+    args.foreach(lift(_, e))
+    e.emit(inst(opcode.Mov, vm.Reg.Rpc, vm.Reg.Pc))
+    e.emit(inst(opcode.Call, name(lambda.ptr)))
+    lift(lambda, e.to(lambda.ptr))
+  case app: tl.App =>
+    call(app.lambda, app.args, e)
+    e.emit(inst(opcode.Mov, vm.Reg.Jmp))
+    args.foreach(lift(_, e))
+    e.emit(inst(opcode.Mov, vm.Reg.Rpc, vm.Reg.Pc))
+    e.emit(inst(opcode.Call))
+    // println(app.lambda)
+    // println(app.args)
+    // e.emit(inst(opcode.Halt))
+    // lift(app.lambda, e)
+    // e.emit(inst(opcode.Halt))
   case _ =>
     /* bad call */
     ???
 
-def load(label: String) =
-  inst(opcode.LoadI32, name(label))
+def load(label: String, e: Emitter) =
+  e.emit(inst(opcode.LoadI32, name(label)))
 
-def store(label: String) =
-  inst(opcode.StoreI32, name(label))
+def store(label: String, e: Emitter) =
+  e.emit(inst(opcode.StoreI32, name(label)))
 
-def cond(cnd: Ir, pas: Ir, fal: Ir) =
+def storev(label: String, v: Ir, e: Emitter) = v match
+  case _: tl.Num => e.emit(inst(opcode.StoreI32, name(label)))
+  case _: tl.Str => ???
+  case _: tl.Id => ???
+  case v: tl.Lambda =>
+    // XXX 1
+    // e.emit(inst(opcode.PushPtr, name(v.ptr)))
+    e.emit(inst(opcode.StorePtr, name(label)))
+  case _: tl.App =>
+    // TODO App's result may not be i32, need to pass ty.Type instead of
+    // typeless Ir.
+    e.emit(inst(opcode.StoreI32, name(label)))
+  case _: tl.Cond =>
+    // TODO Cond's result may not be i32, need to pass ty.Type instead of
+    // typeless Ir.
+    e.emit(inst(opcode.StoreI32, name(label)))
+  case _: tl.Let =>
+    // TODO Let's result may not be i32, need to pass ty.Type instead of
+    // typeless Ir.
+    e.emit(inst(opcode.StoreI32, name(label)))
+
+def cond(cnd: Ir, pas: Ir, fal: Ir, e: Emitter) =
+  val lcond = unique("cond")
+  val lthen = unique("then")
   val lelse = unique("else")
   val ldone = unique("done")
 
-  lift(cnd) ++
-  inst(opcode.Jz, lelse) ++
-  lift(pas) ++
-  inst(opcode.Jmp, ldone) ++
-  inst(opcode.Label, lelse) ++
-  lift(fal) ++
-  inst(opcode.Label, ldone)
+  e.emit(inst(opcode.Label, lcond))
+  lift(cnd, e)
+  e.emit(inst(opcode.Jz, lelse))
+  e.emit(inst(opcode.Label, lthen))
+  lift(pas, e)
+  e.emit(inst(opcode.Jmp, ldone))
+  e.emit(inst(opcode.Label, lelse))
+  lift(fal, e)
+  e.emit(inst(opcode.Label, ldone))
 
-def let(bindings: List[tl.Binding], body: Ir) =
-  bindings.flatMap { case tl.Binding(ast.Id(label, _), value, _) =>
-    lift(value) ++
-    store(label)
-  } ++ lift(body)
+def let(bindings: List[tl.Binding], body: Ir, e: Emitter) =
+  bindings.foreach { case tl.Binding(ast.Id(label, _), value, _) =>
+    lift(value, e)
+    storev(label, value, e)
+  }
+  lift(body, e)
 
-def lambda(params: List[tl.Id], body: Ir) =
-  params.flatMap { case tl.Id(ast.Id(label, _)) =>
-    store(label)
-  } ++
-  lift(body) ++
-  inst(opcode.Ret)
+def lambda(params: List[tl.Id], body: Ir, e: Emitter) =
+  params.reverse.foreach { case tl.Id(ast.Id(label, _)) =>
+    store(label, e)
+  }
+  lift(body, e)
+  e.emit(inst(opcode.Ret))
