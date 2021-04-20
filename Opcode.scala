6519f4e460f14219176f55f4a52aa3060f149deb
Marcos Minond <minond.marcos@gmail.com>
2021-04-19 21:30:34 -0600

    new opcodes

--- Opcode.scala
+++ Opcode.scala
@@ -10,9 +10,12 @@ case object Label extends Opcode
 case object Jz extends Opcode with Print("jz")
 case object Jmp extends Opcode with Print("jmp")
 case object PushI32 extends Opcode with Print("push_i32")
+case object PushPtr extends Opcode with Print("push_ptr")
 case object PushReg extends Opcode with Print("push_reg")
 case object Call extends Opcode with Print("call")
 case object Run extends Opcode with Print("run")
 case object Ret extends Opcode with Print("ret")
+case object Mov extends Opcode with Print("mov")
 case object LoadI32 extends Opcode with Print("load_i32") // local to stack
 case object StoreI32 extends Opcode with Print("store_i32") // stack to local
+case object StorePtr extends Opcode with Print("store_ptr") // stack to local
