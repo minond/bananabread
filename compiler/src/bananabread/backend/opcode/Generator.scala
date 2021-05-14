d9dbc2c49b263271d747ddd3bd4bfc16d19a0bea
Marcos Minond <minond.marcos@gmail.com>
2021-05-14 13:33:36 -0600

    mov takes a string

--- compiler/src/bananabread/backend/opcode/Generator.scala
+++ compiler/src/bananabread/backend/opcode/Generator.scala
@@ -119,7 +119,7 @@ def generateCallWithArgs(scope: Scope, args: List[Ir], call: Instruction): Resul
 
 def generateCallArgsLoad(scope: Scope, args: List[Ir]): Result =
   args.map(generate(scope, _)).squished.map { instructions =>
-    instructions.flatten ++ group(scope, Mov(Pc, Some(value.I32(2))))
+    instructions.flatten ++ group(scope, Mov(Pc, Some("2")))
   }
 
 def generateCond(scope: Scope, cond: Ir, pass: Ir, fail: Ir): Result =
