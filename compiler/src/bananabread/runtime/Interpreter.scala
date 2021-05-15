ef68fcd4ac23ae4e334950812aa265cbf02f03ec
Marcos Minond <minond.marcos@gmail.com>
2021-05-14 21:14:26 -0600

    opcode interpreter prep

---
+++ compiler/src/bananabread/runtime/Interpreter.scala
@@ -0,0 +1,21 @@
+package bananabread
+package runtime
+
+import register._
+import instruction._
+import value.Value
+
+import scala.collection.mutable.Stack
+
+
+sealed trait Dispatch
+case object Halt extends Dispatch
+case object Cont extends Dispatch
+case class Goto(label: String) extends Dispatch
+case class Jump(index: Int) extends Dispatch
+
+
+class Interpreter(codes: List[Code]):
+  val stack = Stack[Value]()
+  val frames = Frames()
+  val registers = Registers()
--- compiler/src/bananabread/runtime/Register.scala
+++ compiler/src/bananabread/runtime/Register.scala
@@ -1,8 +1,28 @@
 package bananabread
 package runtime.register
 
+import runtime.value.I32
+
 
 sealed trait Register
 case object Pc extends Register
 case object Lr extends Register
 case object Jm extends Register
+
+
+class Registers:
+  private var pcVal = I32(0)
+  private var lrVal = I32(0)
+  private var jmVal = I32(0)
+
+  def pc         = pcVal
+  def pc(v: I32) = pcVal = v
+  def pc(i: Int) = pcVal = I32(i)
+
+  def lr         = lrVal
+  def lr(v: I32) = lrVal = v
+  def lr(i: Int) = lrVal = I32(i)
+
+  def jm         = jmVal
+  def jm(v: I32) = jmVal = v
+  def jm(i: Int) = jmVal = I32(i)
