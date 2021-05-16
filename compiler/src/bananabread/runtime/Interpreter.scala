7b5ca9a7c46ddb55f2268ad8efc767d497514335
Marcos Minond <minond.marcos@gmail.com>
2021-05-15 21:53:54 -0600

    machine is now state

--- compiler/src/bananabread/runtime/Interpreter.scala
+++ compiler/src/bananabread/runtime/Interpreter.scala
@@ -16,7 +16,7 @@ case class Jump(index: Int) extends Dispatch
 case class Fatal(msg: String) extends Dispatch
 
 
-case class Machine(
+case class State(
   val stack: Stack[Value],
   val frames: Frames,
   val registers: Registers,
@@ -24,9 +24,9 @@ case class Machine(
   val labels: Map[String, Int],
 )
 
-object Machine:
+object State:
   def from(i: Interpreter) =
-    Machine(i.stack, i.frames, i.registers, i.constants, i.labels)
+    State(i.stack, i.frames, i.registers, i.constants, i.labels)
 
 
 class Interpreter(codes: List[Code], debug: Boolean = false):
@@ -50,7 +50,7 @@ class Interpreter(codes: List[Code], debug: Boolean = false):
     registers.pc.value != -1
 
   def next =
-    handle(codes(registers.pc.value), Machine.from(this)) match
+    handle(codes(registers.pc.value), State.from(this)) match
       case Halt        => registers.pc(-1)
       case Cont        => registers.pc(registers.pc.value + 1)
       case Goto(label) => registers.pc(labels(label))
@@ -70,132 +70,133 @@ class Interpreter(codes: List[Code], debug: Boolean = false):
       println(s"========= FINISH ${codes(registers.pc.value)}")
 
 
-def handle(code: Code, machine: Machine): Dispatch = code match
+def handle(code: Code, state: State): Dispatch = code match
   case _: instruction.Value => Cont
   case _: instruction.Label => Cont
   case instruction.Halt  => Halt
-  case op: Jz    => handleJz(op, machine)
-  case op: Jmp   => handleJmp(op, machine)
-  case op: Push  => handlePush(op, machine)
-  case op: Call  => handleCall(op, machine)
-  case Call0     => handleCall0(machine)
-  case Ret       => handleRet(machine)
-  case Swap      => handleSwap(machine)
-  case op: Mov   => handleMov(op, machine)
-  case op: Load  => handleLoad(op, machine)
-  case op: Store => handleStore(op, machine)
-  case Println   => handlePrintln(machine)
-  case Concat    => handleConcat(machine)
-  case op: Add   => handleAdd(op, machine)
-  case op: Sub   => handleSub(op, machine)
+  case op: Jz    => handleJz(op, state)
+  case op: Jmp   => handleJmp(op, state)
+  case op: Push  => handlePush(op, state)
+  case op: Call  => handleCall(op, state)
+  case Call0     => handleCall0(state)
+  case Ret       => handleRet(state)
+  case Swap      => handleSwap(state)
+  case op: Mov   => handleMov(op, state)
+  case op: Load  => handleLoad(op, state)
+  case op: Store => handleStore(op, state)
+  case Println   => handlePrintln(state)
+  case Concat    => handleConcat(state)
+  case op: Add   => handleAdd(op, state)
+  case op: Sub   => handleSub(op, state)
 
-def handleJz(op: Jz, machine: Machine): Dispatch =
+def handleJz(op: Jz, state: State): Dispatch =
   ???
 
-def handleJmp(op: Jmp, machine: Machine): Dispatch =
+def handleJmp(op: Jmp, state: State): Dispatch =
   ???
 
-def handlePush(op: Push, machine: Machine): Dispatch = op match
+def handlePush(op: Push, state: State): Dispatch = op match
   case Push(I32, v: value.I32) =>
-    machine.stack.push(v)
+    state.stack.push(v)
     Cont
   case Push(Ptr, v: value.Id) =>
-    machine.stack.push(v)
+    state.stack.push(v)
     Cont
   case Push(Scope, value.Id(label)) =>
-    val scope = value.Scope(label, machine.frames.curr)
-    machine.stack.push(scope)
+    val scope = value.Scope(label, state.frames.curr)
+    state.stack.push(scope)
     Cont
   case Push(Const, value.Id(label)) =>
-    machine.constants.get(label) match
+    state.constants.get(label) match
       case Some(v) =>
-        machine.stack.push(v)
+        state.stack.push(v)
         Cont
       case None =>
         Fatal(s"missing const: $label")
   case _ =>
     Fatal("bad push")
 
-def handleCall(op: Call, machine: Machine): Dispatch = machine.frames.curr.get(op.label) match
+def handleCall(op: Call, state: State): Dispatch = state.frames.curr.get(op.label) match
   case None =>
-    machine.frames.next
+    state.frames.next
     Goto(op.label)
   case Some(ptr: value.Id) =>
-    machine.frames.next
+    state.frames.next
     Goto(ptr.label)
   case Some(value.Scope(label, frame)) =>
-    machine.frames.from(frame)
+    state.frames.from(frame)
     Goto(label)
   case Some(_) =>
     Fatal("bad call")
 
-def handleCall0(machine: Machine): Dispatch =
+def handleCall0(state: State): Dispatch =
   ???
 
-def handleRet(machine: Machine): Dispatch = machine.stack.pop match
+def handleRet(state: State): Dispatch = state.stack.pop match
   case value.I32(addr) =>
-    machine.frames.prev
+    state.frames.prev
     Jump(addr)
   case _ =>
     Fatal("bad ret: missing return address")
 
-def handleSwap(machine: Machine): Dispatch =
-  val a = machine.stack.pop
-  val b = machine.stack.pop
-  machine.stack.push(a)
-  machine.stack.push(b)
+def handleSwap(state: State): Dispatch =
+  val a = state.stack.pop
+  val b = state.stack.pop
+  state.stack.push(a)
+  state.stack.push(b)
   Cont
 
-def handleMov(op: Mov, machine: Machine): Dispatch = op match
+def handleMov(op: Mov, state: State): Dispatch = op match
   case Mov(reg, Some(offset)) =>
-    val curr = machine.registers.get(reg)
+    val curr = state.registers.get(reg)
     val next = value.I32(curr.value + offset.value)
-    machine.registers.set(reg, next)
+    state.registers.set(reg, next)
     Cont
-  case Mov(reg, None) => machine.stack.pop match
+  case Mov(reg, None) => state.stack.pop match
     case addr: value.I32 =>
-      machine.registers.set(reg, addr)
+      state.registers.set(reg, addr)
       Cont
     case value.Id(label) =>
-      machine.labels.get(label) match
+      state.labels.get(label) match
         case Some(i) =>
-          machine.registers.set(reg, i)
+          state.registers.set(reg, i)
           Cont
         case None =>
           Fatal(s"bad mov: missing label $label")
     case value.Scope(label, frame) =>
-      machine.labels.get(label) match
+      state.labels.get(label) match
         case Some(i) =>
-          machine.frames.from(frame)
-          machine.registers.set(reg, value.I32(i))
+          state.frames.from(frame)
+          state.registers.set(reg, value.I32(i))
           Cont
         case None =>
           Fatal(s"bad mov: missing scope $label")
     case _ =>
       Fatal("bad mov: invalid stack entry")
 
-def handleLoad(op: Load, machine: Machine): Dispatch = machine.frames.curr.get(op.label) match
+def handleLoad(op: Load, state: State): Dispatch = state.frames.curr.get(op.label) match
   case None =>
-    val value = machine.constants(op.label)
-    machine.stack.push(value)
+    println(s"loading ${op}")
+    val value = state.constants(op.label)
+    state.stack.push(value)
     Cont
   case Some(v) =>
-    machine.stack.push(v)
+    state.stack.push(v)
     Cont
 
-def handleStore(op: Store, machine: Machine): Dispatch =
-  machine.frames.curr.put(op.label, machine.stack.pop)
+def handleStore(op: Store, state: State): Dispatch =
+  state.frames.curr.put(op.label, state.stack.pop)
   Cont
 
-def handlePrintln(machine: Machine): Dispatch =
-  println(machine.stack.head)
+def handlePrintln(state: State): Dispatch =
+  println(state.stack.head)
   Cont
 
-def handleConcat(machine: Machine): Dispatch =
+def handleConcat(state: State): Dispatch =
   ???
 
-def handleAdd(op: Add, machine: Machine): Dispatch =
+def handleAdd(op: Add, state: State): Dispatch =
   ???
 
-def handleSub(op: Sub, machine: Machine): Dispatch =
+def handleSub(op: Sub, state: State): Dispatch =
   ???
