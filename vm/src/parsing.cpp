b991210b59c859e84e82a037d2e9d651d633995b
Marcos Minond <minond.marcos@gmail.com>
2021-08-02 14:31:09 -0600

    smaller parsing header

--- vm/src/parsing.cpp
+++ vm/src/parsing.cpp
@@ -7,24 +7,6 @@
 namespace Bananabread {
 namespace Parsing {
 
-void parse(const std::string& program) {
-  auto lines = split_lines(program);
-
-  for (auto line : lines) {
-    parse_line(line);
-  }
-}
-
-void parse_line(const std::string& line) {
-    std::cout << "parsing " << line << std::endl;
-
-    auto words = split_words(line);
-
-    for (auto word : words) {
-      std::cout << word << std::endl;
-    }
-}
-
 std::vector<std::string> split_lines(const std::string& str) {
   auto lines = std::vector<std::string>();
   auto ss = std::stringstream(str);
@@ -40,6 +22,24 @@ std::vector<std::string> split_words(const std::string& str) {
   auto words = std::vector<std::string>();
 
   return words;
+}
+
+void parse_line(const std::string& line) {
+    std::cout << "parsing " << line << std::endl;
+
+    auto words = split_words(line);
+
+    for (auto word : words) {
+      std::cout << word << std::endl;
+    }
+}
+
+void parse(const std::string& program) {
+  auto lines = split_lines(program);
+
+  for (auto line : lines) {
+    parse_line(line);
+  }
 }
 
 } // namespace Parsing
--- vm/src/parsing.hpp
+++ vm/src/parsing.hpp
@@ -5,9 +5,5 @@ namespace Parsing {
 
 void parse(const std::string& program);
 
-void parse_line(const std::string& line);
-std::vector<std::string> split_lines(const std::string& str);
-std::vector<std::string> split_words(const std::string& str);
-
 } // namespace Parsing
 } // namespace Bananabread
