f75bb8163b54b19a929fcd6c0604f0a2bf579d06
Marcos Minond <marcos.minond@mx.com>
2021-08-30 20:18:34 -0600

    ebp register

--- compiler/src/bananabread/runtime/Register.scala
+++ compiler/src/bananabread/runtime/Register.scala
@@ -6,7 +6,8 @@ import runtime.value.{Value, I32, Pointer, Ptr, Nullptr}
 
 sealed trait Register
 case object Pc extends Register
-case object Esp extends Register
+case object Esp extends Register // ESP is the current stack pointer
+case object Ebp extends Register // EBP is the base pointer for the current stack frame
 case object Lr extends Register
 case object Jm extends Register
 case object Rt extends Register
@@ -15,6 +16,7 @@ case object Rt extends Register
 class Registers:
   private var pcVal  = I32(0)
   private var espVal = I32(0)
+  private var ebpVal = I32(0)
   private var lrVal  = I32(0)
   private var jmVal  = I32(0)
   private var rtVal: Value = Nullptr
@@ -26,6 +28,10 @@ class Registers:
   def esp         = espVal
   def esp(v: I32) = espVal = v
   def esp(i: Int) = espVal = I32(i)
+
+  def ebp         = ebpVal
+  def ebp(v: I32) = ebpVal = v
+  def ebp(i: Int) = ebpVal = I32(i)
 
   def lr          = lrVal
   def lr(v: I32)  = lrVal = v
@@ -42,6 +48,7 @@ class Registers:
     case Rt  => throw new Exception("invalid operation on rt register")
     case Pc  => pc(pcVal.value + 1)
     case Esp => esp(espVal.value + 1)
+    case Ebp => ebp(ebpVal.value + 1)
     case Lr  => lr(lrVal.value + 1)
     case Jm  => jm(jmVal.value + 1)
 
@@ -49,6 +56,7 @@ class Registers:
     case Rt  => throw new Exception("invalid operation on rt register")
     case Pc  => pc(pcVal.value - 1)
     case Esp => esp(espVal.value - 1)
+    case Ebp => ebp(ebpVal.value - 1)
     case Lr  => lr(lrVal.value - 1)
     case Jm  => jm(jmVal.value - 1)
 
@@ -56,6 +64,7 @@ class Registers:
     case Rt  => throw new Exception("invalid operation on rt register")
     case Pc  => pc
     case Esp => esp
+    case Ebp => ebp
     case Lr  => lr
     case Jm  => jm
 
@@ -64,8 +73,9 @@ class Registers:
     case Rt  => throw new Exception("invalid operation on rt register")
     case Pc  => pc(v)
     case Esp => esp(v)
+    case Ebp => ebp(v)
     case Lr  => lr(v)
     case Jm  => jm(v)
 
   override def toString: String =
-    f"pc => ${pc.value}, esp => $esp, lr => ${lr.value}, jm => ${jm.value}, rt => $rt"
+    f"pc => ${pc.value}, esp => $esp, ebp -> $ebp, lr => ${lr.value}, jm => ${jm.value}, rt => $rt"
