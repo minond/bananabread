528227d24f38164ee064e0668992350212c1b957
Marcos Minond <marcos.minond@mx.com>
2021-09-15 20:28:00 -0600

    Handling exceptions and handled runtime errors

--- compiler/src/bananabread/Main.scala
+++ compiler/src/bananabread/Main.scala
@@ -29,7 +29,7 @@ def main(args: Array[String]) =
   val doPrintTypes = flagPrintTypes || flagDebug
   val doPrintState = flagPrintState || flagDebug
 
-  val res =
+  val res = Try {
     for
       ast <- parse(fileName, code)
       _    = if doPrintAst then
@@ -55,23 +55,20 @@ def main(args: Array[String]) =
                println("~~~~~~~~~~~~~~~~~~~~~~~")
 
       vm   = Interpreter(ins)
+      _   <- if doDebug
+             then vm.debugging.stepping.run
+             else vm.run
     yield
-      if doDebug then
-        Try { vm.debugging.stepping.run } match
-          case Success(_)  =>
-          case Failure(ex: Exception) if ex.getMessage == "quit" =>
-          case Failure(ex) =>
-            throw ex
-      else
-        vm.run
-
       vm
+  }
 
   res match
-    case Right(vm) =>
+    case Failure(ex: Exception) if ex.getMessage == "quit" =>
+    case Failure(ex) => throw ex
+    case Success(Left(err: Errors)) => println(errpp(err, code))
+    case Success(Left(err)) => println(s"unhandled error: $err")
+    case Success(Right(vm)) =>
       if doPrintState then
         println("~~~~~~~~~~~~~~~~~~~~~~~")
         vm.printState
         println("~~~~~~~~~~~~~~~~~~~~~~~")
-    case Left(err: Errors) => println(errpp(err, code))
-    case Left(err) => println(s"unhandled error: $err")
