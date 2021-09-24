4faafb8ee9872023be573f921637b6253ecd90b1
Marcos Minond <minond.marcos@gmail.com>
2021-09-23 19:09:10 -0600

    printer package

--- compiler/src/bananabread/printer/Errors.scala
+++ compiler/src/bananabread/printer/Errors.scala
@@ -1,19 +1,20 @@
 package bananabread
-package error
-
-import backend.opcode.error => genop
-import parsing.error => parse
-import runtime.error => runtime
-import typechecker.error => typechecker
-import ir.typeless.error => typelessIr
-import ir.typed.error => typedIr
+package printer
 
 import ir.typed
 import ir.typeless
-import parsing.location.Location
 import parsing.ast
-import parsing.ast.{Def, Expr}
-import bananabread.runtime.instruction.{Instruction, Code, Label, Value, pp => inspp}
+import parsing.location.Location
+import runtime.instruction
+import runtime.instruction.pp => inspp
+
+import error.Err
+import backend.opcode.error => genopErr
+import parsing.error => parseErr
+import runtime.error => runtimeErr
+import typechecker.error => typecheckerErr
+import ir.typeless.error => typelessIrErr
+import ir.typed.error => typedIrErr
 
 import scala.io.AnsiColor.{BOLD, RESET}
 
@@ -24,137 +25,144 @@ val OpcodePadding = 10
 
 
 def pp(err: Err, source: String): String = err match
-  case parse.BadNumErr(_, location) =>
+  case parseErr.BadNumErr(_, location) =>
     lines(
       generateSyntaxErrorLine(s"bad number", location, source),
       isolateBadLine(location, source),
     )
 
-  case parse.UnclosedStringErr(location) =>
+  case parseErr.UnclosedStringErr(location) =>
     lines(
       generateSyntaxErrorLine(s"bad string", location, source),
       isolateBadLine(location, source),
     )
 
-  case parse.EmptyBeginNotAllowedErr(token) =>
+  case parseErr.EmptyBeginNotAllowedErr(token) =>
     lines(
       generateSyntaxErrorLine(s"empty begin", token.location, source),
       isolateBadLine(token.location, source),
     )
 
-  case parse.BadOperatorDefinitionErr(location) =>
+  case parseErr.BadOperatorDefinitionErr(location) =>
     lines(
       generateSyntaxErrorLine(s"bad operator definition", location, source),
       isolateBadLine(location, source),
     )
 
-  case parse.UnexpectedEofErr(prev) =>
+  case parseErr.UnexpectedEofErr(prev) =>
     lines(
       generateSyntaxErrorLine(s"unexpected <eof> after `$prev`", prev.location, source),
       isolateBadLine(prev.location, source),
     )
 
-  case parse.UnexpectedTokenErr(token) =>
+  case parseErr.UnexpectedTokenErr(token) =>
     lines(
       generateSyntaxErrorLine(s"unexpected token `$token` found", token.location, source),
       isolateBadLine(token.location, source),
     )
 
-  case parse.MissingExpectedTokenErr(token, expected) =>
+  case parseErr.MissingExpectedTokenErr(token, expected) =>
     lines(
       generateSyntaxErrorLine(s"expected `$expected` but found `$token`", token.location, source),
       isolateBadLine(token.location, source),
     )
 
-  case parse.MissingExpectedTokenAfterErr(after, token, expected) =>
+  case parseErr.MissingExpectedTokenAfterErr(after, token, expected) =>
     lines(
       generateSyntaxErrorLine(s"expected `$expected` after `$after` but found `$token`", token.location, source),
       isolateBadLine(after.location, source),
     )
 
-  case genop.BadPushErr(_, node) =>
+  case genopErr.BadPushErr(_, node) =>
     node.expr match
-      case expr : Expr =>
+      case expr: ast.Expr =>
         lines(
           generateRuntimeErrorLine(s"bad push, `${expr}` is not a valid runtime value", expr.location, source),
           isolateBadLine(expr.location, source),
         )
-      case stmt : Def =>
+      case stmt: ast.Def =>
         lines(
           generateRuntimeErrorLine(s"bad push, `${stmt.value}` is not a valid runtime value", stmt.value.location, source),
           isolateBadLine(stmt.value.location, source),
         )
-      case _ : (ast.Module | ast.Import) =>
+      case _: (ast.Module | ast.Import) =>
         ???
 
-  case genop.UndeclaredIdentifierErr(id) =>
+  case genopErr.UndeclaredIdentifierErr(id) =>
     lines(
       generateRuntimeErrorLine(s"${id.expr.lexeme} was referenced but not found", id.expr.location, source),
       isolateBadLine(id.expr.location, source),
     )
 
-  case genop.LookupErr(id) =>
+  case genopErr.LookupErr(id) =>
     lines(
       generateRuntimeErrorLine(s"${id.lexeme} was referenced but not found", id.location, source),
       isolateBadLine(id.location, source),
     )
 
-  case genop.UnknownUserOpcodeErr(code, loc) =>
+  case genopErr.UnknownUserOpcodeErr(code, loc) =>
     lines(
       generateSyntaxErrorLine(s"invalid opcode", loc, source),
       isolateBadLine(code.location, source),
     )
 
-  case genop.BadCallErr(typed.Id(id, _)) =>
+  case genopErr.BadCallErr(typed.Id(id, _)) =>
     lines(
       generateRuntimeErrorLine(s"bad call to `${id.lexeme}`", id.location, source),
       isolateBadLine(id.location, source),
     )
 
-  case genop.BadCallErr(node) =>
+  case genopErr.BadCallErr(node) =>
     lines(
       generateRuntimeErrorLine(s"bad call to value of kind ${node.getClass.getSimpleName}", node.expr.location, source),
       isolateBadLine(node.expr.location, source),
     )
 
-  case runtime.RuntimeErr(msg, ins, codes, registers) =>
+  case runtimeErr.RuntimeErr(msg, ins, codes, registers) =>
     lines(
       generateOpcodeErrorLine(msg),
       isolateBadOpcode(registers.pc.value, codes),
     )
 
-  case typechecker.UnknowTypeErr(tag) =>
+  case typecheckerErr.UnknowTypeErr(tag) =>
     lines(
       generateTypeErrorLine(s"unknown type `${tag.id.lexeme}`", tag.id.location, source),
       isolateBadLine(tag.id.location, source),
     )
 
-  case typechecker.LookupErr(label, ir) =>
+  case typecheckerErr.LookupErr(label, ir) =>
     lines(
       generateTypeErrorLine(s"`${label}` was referenced but not found", ir.expr.location, source),
       isolateBadLine(ir.expr.location, source),
     )
 
-  case typechecker.UnunifiedTypeVarErr(_, ir) =>
+  case typecheckerErr.UnunifiedTypeVarErr(_, ir) =>
     lines(
       generateTypeErrorNearLine(s"unable to unify expression, perhaps a type annotation is missing", ir.expr.location, source),
       isolateBadLine(ir.expr.location, source),
     )
 
-  case typechecker.UnificationErr(expected, got, ir) =>
+  case typecheckerErr.UnificationErr(expected, got, ir) =>
     lines(
       generateTypeErrorLine(s"expected value of type `$expected` but got value of type `$got`", ir.expr.location, source),
       isolateBadLine(ir.expr.location, source),
     )
 
-  case typedIr.UndeclaredIdentifierErr(typeless.Id(id)) =>
+  case typedIrErr.UndeclaredIdentifierErr(typeless.Id(id)) =>
     lines(
       generateTypeErrorLine(s"undeclared identifier `$id`", id.location, source),
       isolateBadLine(id.location, source),
     )
 
+  case typelessIrErr.LiftinTheUnliftableErr(node) =>
+    lines(
+      generateInternalErrorLine(s"unable to process code", node.location, source),
+      isolateBadLine(node.location, source),
+    )
+
   case _ =>
     s"${err.getClass.getCanonicalName}: ${err}"
+
 
 def generateSyntaxErrorLine(message: String, loc: Location, source: String) =
   s"${BOLD}syntax error: ${message} in ${generateCoordinates(loc, source)}${RESET}"
@@ -171,9 +179,14 @@ def generateTypeErrorLine(message: String, loc: Location, source: String) =
 def generateTypeErrorNearLine(message: String, loc: Location, source: String) =
   s"${BOLD}type error: ${message} near ${generateCoordinates(loc, source)}${RESET}"
 
+def generateInternalErrorLine(message: String, loc: Location, source: String) =
+  s"${BOLD}internal error: ${message} in ${generateCoordinates(loc, source)}${RESET}"
+
+
 def generateCoordinates(loc: Location, source: String) =
   val (row, col) = offsetToRowAndCol(loc.offset, source)
   s"${loc.source}:${row + 1}:${col + 1}"
+
 
 def offsetToRowAndCol(offset: Int, source: String): (Int, Int) =
   source.split("\n").foldLeft[(Int, Int)]((0, offset)) {
@@ -196,16 +209,16 @@ def isolateBadLine(loc: Location, source: String) =
       else acc :+ withLineNumber(max, index, line)
   }.mkString("\n")
 
-def isolateBadOpcode(pc: Int, codes: List[Code]) =
+def isolateBadOpcode(pc: Int, codes: List[instruction.Code]) =
   val max = codes.size
   val bads = codes.zipWithIndex.slice(pc - OpcodePadding, pc + OpcodePadding)
   val pointer = generatePointer(0)
   bads.foldLeft[List[String]](List.empty) {
     case (acc, (line, index)) =>
       val code = line match
-        case op: Value       => inspp(op)
-        case op: Label       => s"    ${inspp(op)}"
-        case op: Instruction => s"        ${inspp(op)}"
+        case op: instruction.Value       => inspp(op)
+        case op: instruction.Label       => s"    ${inspp(op)}"
+        case op: instruction.Instruction => s"        ${inspp(op)}"
       val formatted =
         if index == pc
         then s"${BOLD}${code}${RESET}"
