4b4723290e729a404fb65489a6d5c0e512b01e82
Marcos Minond <minond.marcos@gmail.com>
2021-09-23 19:33:04 -0600

    Overloading formatting

--- compiler/src/bananabread/printer/Printer.scala
+++ compiler/src/bananabread/printer/Printer.scala
@@ -5,9 +5,6 @@ import runtime.instruction.Code
 import error.Err
 
 
-def pp(codes: List[Code]): String =
-  instructions.pp(codes)
-def pp(code: Code, align: Boolean = false): String =
-  instructions.pp(code, align)
-def pp(err: Err, source: String): String = 
-  errors.pp(err, source)
+def pp(codes: List[Code]): String                   = instructions.pp(codes)
+def pp(code: Code, align: Boolean = false): String  = instructions.pp(code, align)
+def pp(err: Err, source: String): String            = errors.pp(err, source)
