2f7c1991ac0dca57e1add12a3c8f55970f1baf7f
Marcos Minond <marcos.minond@mx.com>
2021-10-07 23:08:37 -0600

    Removing unused type

--- compiler/src/bananabread/typechecker/Error.scala
+++ compiler/src/bananabread/typechecker/Error.scala
@@ -11,7 +11,6 @@ sealed trait InferenceErr
 case class LookupErr(label: String, ir: Ir) extends InferenceErr
 case class TypeMismatchErr(expected: Type, got: Type, node: Ir) extends InferenceErr
 case class GenTypeMismatchErr[T](expected: Class[T], got: Type, node: Ir) extends InferenceErr
-case class UnexpectedTypeErr[Expected <: Type](got: Type, node: Ir) extends InferenceErr
 case class UnknowTypeErr(tag: ast.TyId) extends InferenceErr
 case class UnunifiedTypeVarErr(v: Var, node: Ir) extends InferenceErr
 case class UnificationErr(expected: Type, got: Type, node: Ir) extends InferenceErr
