5cec49bcb3c461a668e09c14cb721f7084c5d236
Marcos Minond <minond.marcos@gmail.com>
2022-05-22 22:41:08 -0600

    Using old compiler

--- compiler/lib/Sample.bb
+++ compiler/lib/Sample.bb
@@ -6,9 +6,21 @@ operator(infix, 2, '-)
 operator(infix, 2, '+)
 operator(infix, 2, '++)
 
-println("testing")
+// println("testing")
 
-def x(n) = n
-def y = x
+// def x(n) = n
+// def y = x
 
 // println(y("head") ++ "tail")
+
+// def x = 1
+
+def x = 123
+
+println(x + 1)
+
+// %[[
+//   println
+// ]]
+
+// let x = 1 in x
--- compiler/src/bananabread/Main.scala
+++ compiler/src/bananabread/Main.scala
@@ -20,45 +20,45 @@ def main(args: Array[String]) =
   val doPrintOpcodes = flagPrintOpcodes || flagDebug
   val doPrintState = flagPrintState || flagDebug
 
-  // val res = Try {
-  //   for
-  //     sources <- loader.load(fileName, code)
-  //     ir <- program.lift(sources.head)
-  //     ins <- backend.bytecode.compile(ir)
-  //     _    = if doPrintOpcodes then
-  //              println("~~~~~~~~~~~~~~~~~~~~~~~")
-  //              println(pp(ins))
-  //              println("~~~~~~~~~~~~~~~~~~~~~~~")
-  //
-  //     vm   = Interpreter(ins)
-  //     _   <- if doDebug
-  //            then vm.debugging.stepping.run
-  //            else vm.run
-  //   yield
-  //     vm
-  // }
-  //
-  // res match
-  //   case Failure(ex: Exception) if ex.getMessage == "quit" =>
-  //   case Failure(ex) => throw ex
-  //   case Success(Left(err: Err)) => println(pp(err, code))
-  //   case Success(Left(err)) => println(s"unhandled error: $err")
-  //   case Success(Right(vm)) =>
-  //     if doPrintState then
-  //       println("~~~~~~~~~~~~~~~~~~~~~~~")
-  //       vm.printState
-  //       println("~~~~~~~~~~~~~~~~~~~~~~~")
-
-  val res =
+  val res = Try {
     for
       sources <- loader.load(fileName, code)
-      nodes <- program.lift(sources.head)
-      res = treewalker.run(nodes)
+      ir <- program.lift(sources.head)
+      ins <- backend.bytecode.compile(ir)
+      _    = if doPrintOpcodes then
+               println("~~~~~~~~~~~~~~~~~~~~~~~")
+               println(pp(ins))
+               println("~~~~~~~~~~~~~~~~~~~~~~~")
+
+      vm   = Interpreter(ins)
+      _   <- if doDebug
+             then vm.debugging.stepping.run
+             else vm.run
     yield
-      res
+      vm
+  }
 
   res match
-    case Left(err: Err) => println(pp(err, code))
-    case Left(err) => println(s"unhandled error: $err")
-    case Right(walker) =>
-      println(walker)
+    case Failure(ex: Exception) if ex.getMessage == "quit" =>
+    case Failure(ex) => throw ex
+    case Success(Left(err: Err)) => println(pp(err, code))
+    case Success(Left(err)) => println(s"unhandled error: $err")
+    case Success(Right(vm)) =>
+      if doPrintState then
+        println("~~~~~~~~~~~~~~~~~~~~~~~")
+        vm.printState
+        println("~~~~~~~~~~~~~~~~~~~~~~~")
+
+  // val res =
+  //   for
+  //     sources <- loader.load(fileName, code)
+  //     nodes <- program.lift(sources.head)
+  //     res = treewalker.run(nodes)
+  //   yield
+  //     res
+  //
+  // res match
+  //   case Left(err: Err) => println(pp(err, code))
+  //   case Left(err) => println(s"unhandled error: $err")
+  //   case Right(walker) =>
+  //     println(walker)
